754239249de3c70ac2dbdb9b7e99410f6ab39d93
kosclid <kos-2000@hanmail.net>
2022-10-26 14:16:48 +0900

    중가관리1

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)  # 제목 저장
+    content = models.TextField()  # 내용 저장 
+
+    created_at = models.DateTimeField()  # 생성된 시각 저장
+
+    def __str__(self):
+        return self.title
+
+
+
