d1f896a2a588f059fa611c882a267296d9cf7886
kosclid <kos-2000@hanmail.net>
2022-10-28 11:40:39 +0900

    "다이어리 편집, 삭제 기능 추가"

--- diary/urls.py
+++ diary/urls.py
@@ -5,7 +5,8 @@ urlpatterns = [
         path('', views.index),
         path('<int:pk>/', views.dia_detail),
         path('new/', views.dia_new),
-
+        path('<int:pk>/edit/', views.dia_edit),
+        path('<int:pk>/delete/', views.dia_delete),
 
 ]
 
--- diary/views.py
+++ diary/views.py
@@ -8,21 +8,23 @@ from django.views.generic import CreateView
 def index(request):
     dia_qs = Memory.objects.all().order_by('-pk')
     return render(
-        request, 
+        request,
         "diary/memory_list.html",
-        {'dia_list' : dia_qs,
-        })
+        {'dia_list': dia_qs,
+         })
+
 
 def dia_detail(request, pk):
-    dtail = Memory.objects.get(pk = pk)
+    dtail = Memory.objects.get(pk=pk)
     return render(
-        request, 
+        request,
         "diary/memory_detail.html",
-        {'dtl' : dtail,
-        })
+        {'dtl': dtail,
+         })
+
 
 def dia_new(request):
-    if request.method =="GET":
+    if request.method == "GET":
         form = DiaForm()  # 인자 없이 주면 빈 폼이다.
     else:
         form = DiaForm(request.POST)  # 입력된 값은 request.POST에 저장 되어있다.
@@ -34,5 +36,33 @@ def dia_new(request):
             return redirect(memory)
 
     return render(request, 'diary/memory_new.html', {
-        'form' : form,
+        'form': form,
+    })
+
+
+def dia_edit(request, pk):
+    memory = Memory.objects.get(pk=pk)
+    if request.method == "GET":
+        form = DiaForm(instance=memory)  # 인자 없이 주면 빈 폼이다.
+    else:
+        form = DiaForm(request.POST, instance=memory)  # 입력된 값은 request.POST에 저장 되어있다.
+        if form.is_valid():
+            memory = form.save()  # ModelForm에서 지원
+            return redirect(memory)
+
+    return render(request, 'diary/memory_new.html', {
+        'form': form,
+    })
+
+
+def dia_delete(request, pk):
+    memory = Memory.objects.get(pk=pk)
+
+    # ToDo: delete memory
+    if request.method =="POST":
+        memory.delete()
+        return redirect('/diary/')
+
+    return render(request, "diary/memory_confirm_delete.html", {
+        'memory': memory
     })
