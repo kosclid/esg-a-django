a9d3fb742087b35511eecb4a54f88d5f21339bba
kosclid <kos-2000@hanmail.net>
2022-10-28 16:17:14 +0900

    로그인 로그아웃, 회원가입 구현

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from accounts import views
 
 urlpatterns = [
-
+    path("login/", views.login),
+    path("logout/", views.logout),
+    path("profile/", views.profile),
+    path("signup/", views.signup),
 ]
-
